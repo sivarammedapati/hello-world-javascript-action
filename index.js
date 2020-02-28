15f76be277265e8e6ac145049878b8717549093a
sivarammedapati <sivaram.medapati@gmail.com>
2020-02-28 18:12:23 +0530

    new update

--- index.js
+++ index.js
@@ -1,15 +1,11 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 
-const child_process = require('child_process');
-const fs = require('fs');
-const process = require('process');
-
-const get_gha_input = (name) => { return process.env[`INPUT_${name.toUpperCase()}`]; };
-console.log(get_gha_input);
 try {
   // `who-to-greet` input defined in action metadata file
   //console.log(process.env);
+  console.log(process.env);
+  console.log(core);
   const nameToGreet = core.getInput('who-to-greet');
   const wishPerson = core.getInput('wish-person')
   console.log(`Hello ${nameToGreet}!`);
