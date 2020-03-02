2ae2ac55b748c55ce14e43da0249947d169d318b
sivarammedapati <sivaram.medapati@gmail.com>
2020-03-02 15:50:29 +0530

    bot one

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ try {
   // Get the JSON webhook payload for the event that triggered the workflow
   //const payload = JSON.stringify(github.context.payload, undefined, 2)
   const payload = JSON.parse(JSON.stringify(github.context.payload, undefined, 2));
-  const commits = JSON.parse(JSON.stringify(payload.commits), undefined, 2);
+  const commits = JSON.parse(payload.commits);
   core.setOutput("payload", JSON.stringify(commits.message));
 
   //console.log(`The event payload: ${payload}`);
