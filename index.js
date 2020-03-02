6be62623c9c16d0f522f6e0cad4810e7cb3f3183
sivarammedapati <sivaram.medapati@gmail.com>
2020-03-02 16:01:57 +0530

    bot six

--- index.js
+++ index.js
@@ -19,8 +19,9 @@ try {
   // Get the JSON webhook payload for the event that triggered the workflow
   //const payload = JSON.stringify(github.context.payload, undefined, 2)
   const payload = JSON.parse(JSON.stringify(github.context.payload, undefined, 2));
-  const commits = JSON.parse(payload.commits);
-  core.setOutput("payload", JSON.stringify(commits));
+  const commits = JSON.parse(JSON.stringify(payload.commits));
+  console.log(commits['message'])
+  core.setOutput("payload", commits['message']);
 
   //console.log(`The event payload: ${payload}`);
 } catch (error) {
