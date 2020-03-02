d363cf92f6003de967a75c314c9f2c69752f3145
sivarammedapati <sivaram.medapati@gmail.com>
2020-03-02 12:38:58 +0530

    new update

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ try {
   console.log(day);
   // Get the JSON webhook payload for the event that triggered the workflow
   const payload = JSON.stringify(github.context.payload, undefined, 2)
-  //console.log(`The event payload: ${payload}`);
+  console.log(`The event payload: ${payload}`);
 } catch (error) {
   core.setFailed(error.message);
 }
